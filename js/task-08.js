957fb3bc1b0af4d3c461c7d39729798df90851e6
YuriiTrukhin <68285301+YuriiTrukhin@users.noreply.github.com>
2020-10-18 21:11:29 +0300

    восьмая задача

--- js/task-08.js
+++ js/task-08.js
@@ -1,32 +1,38 @@
 const controls = document.querySelector("div#controls");
-const input = document.querySelector("div>input");
 const createBtn =document.querySelector('div#controls button[data-action="render"]');
 const destroyBtn =document.querySelector('div#controls button[data-action="destroy"]');
 const box = document.getElementById("boxes");
-let acc =0;
-
-input.addEventListener("input",()=>input.value)
 
 
-input.addEventListener("input",()=> {
-let div = document.createElement("div");
-createBtn.addEventListener("click",()=>{
-    box.append(div)
-    let a = Math.floor(Math.random()*250)
-    
-    let c=30
-    let b=30
-    if(input.value<2){
-    div.style.width ="30px";
-    div.style.height ="30px";
-    } else {
-    div.style.width +=`${c}px`;
-    div.style.height +=`${b}px`;        
-    }
-    div.style.background =`rgb(${a+3},${a-3},${a+5})`;
-});  
-})
-destroyBtn.addEventListener("click",()=>box.innerHTML = '')
+
+function getAmount() {
+  let amount = +document.querySelector("#controls input").value;
+  createBoxes(amount);
+}
+
+function createBoxes(amount) {
+  let basicSize = 30;
+  var fragment = document.createDocumentFragment();
+  for (let i = 0; i < amount; i++) {
+    let size = basicSize + i * 10;
+    let div = document.createElement("div");
+    div.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgba( ${random()} , ${random()} , ${random()} )`;
+    fragment.appendChild(div);
+  }
+  boxes.appendChild(fragment);
+}
+
+// input.addEventListener("input",()=> {
+// let div = document.createElement("div");
+// createBtn.addEventListener("click",()=>{
+//     box.append(div)
+//     let a = Math.floor(Math.random()*100)
+//   for (let i = 0; i < amount; i++) {
+//     let size = basicSize + i * 10;    
+//     div.style.background =`rgb(${a+3},${a-3},${a+5})`;
+// });  
+// })
+// destroyBtn.addEventListener("click",()=>box.innerHTML = '')
 
 
 

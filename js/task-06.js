40f2d20e40ba4aea2f36ecb69ab60c8ef4d39071
YuriiTrukhin <68285301+YuriiTrukhin@users.noreply.github.com>
2020-10-18 16:26:31 +0300

    исправляет 6 задачу

--- js/task-06.js
+++ js/task-06.js
@@ -1,19 +1,19 @@
 const inputItem = document.body.querySelector("input#validation-input");
-const maxLength = inputItem.getAttribute("data-length")
+const maxLength = Number(inputItem.getAttribute("data-length"))
 
+console.log(typeof maxLength);
 
 inputItem.addEventListener("blur",()=>{
-    
-if(inputItem.value.length<=maxLength){
+if (inputItem.value.length>maxLength||inputItem.value.length<maxLength){
+    inputItem.classList.remove("valid")
+    inputItem.classList.add("invalid")
+}    
+if(inputItem.value.length===maxLength){
     inputItem.classList.remove("invalid")
     inputItem.classList.add("valid")
 } 
 if(inputItem.value.length===0){
     inputItem.classList.remove("valid")
     inputItem.classList.remove("invalid")    
-}
-if (inputItem.value.length>maxLength&&inputItem.value.length!==0){
-    inputItem.classList.remove("valid")
-    inputItem.classList.add("invalid")
 }
 })  

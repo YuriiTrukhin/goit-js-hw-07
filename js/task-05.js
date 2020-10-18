a1629c62f026a084b239c9fcbef8b4fb5297da35
YuriiTrukhin <68285301+YuriiTrukhin@users.noreply.github.com>
2020-10-18 16:52:22 +0300

    добавил див на седьмую дз

--- js/task-05.js
+++ js/task-05.js
@@ -2,10 +2,10 @@
 const input = document.body.querySelector("input#name-input")
 const span = document.body.querySelector("span#name-output")
 
-input.oninput = function(){
-    if(input.value === ""){
-    span.textContent = "незнакомец";
+input.oninput = function () {
+    if (input.value === "") {
+        span.textContent = "незнакомец";
     } else {
-            span.textContent =`${input.value}`;
-            }
+        span.textContent = `${input.value}`;
+    }
 }
--- js/task-06.js
+++ js/task-06.js
@@ -1,19 +1,17 @@
 const inputItem = document.body.querySelector("input#validation-input");
-const maxLength = Number(inputItem.getAttribute("data-length"))
-
-console.log(typeof maxLength);
+const maxLength = Number(inputItem.getAttribute("data-length"));
 
 inputItem.addEventListener("blur",()=>{
 if (inputItem.value.length>maxLength||inputItem.value.length<maxLength){
-    inputItem.classList.remove("valid")
-    inputItem.classList.add("invalid")
+    inputItem.classList.remove("valid");
+    inputItem.classList.add("invalid");
 }    
 if(inputItem.value.length===maxLength){
-    inputItem.classList.remove("invalid")
-    inputItem.classList.add("valid")
+    inputItem.classList.remove("invalid");
+    inputItem.classList.add("valid");
 } 
 if(inputItem.value.length===0){
-    inputItem.classList.remove("valid")
-    inputItem.classList.remove("invalid")    
+    inputItem.classList.remove("valid");
+    inputItem.classList.remove("invalid");    
 }
 })  

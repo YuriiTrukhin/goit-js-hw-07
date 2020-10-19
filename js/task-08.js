93462c8945f3cfb65ac14abfa0a72cf34e9084ac
YuriiTrukhin <68285301+YuriiTrukhin@users.noreply.github.com>
2020-10-19 17:15:17 +0300

    исправил цвета коробочек

--- js/task-08.js
+++ js/task-08.js
@@ -12,14 +12,15 @@ function getAmount() {
 
 function createBoxes(amount) {
   let basicSize = 30;
-  let color = Math.floor(Math.random() * 256);
-  let colorTwo = Math.floor(Math.random() * 256);
-  let colorThree = Math.floor(Math.random() * 256);
+  
   let div;
   for (let i = 0; i < amount; i++) {
     div = document.createElement("div");
-    let size = basicSize + i * 10;    
+    let size = basicSize + i * 10;
+    let color = Math.floor(Math.random() * 256);
+  let colorTwo = Math.floor(Math.random() * 256);
+  let colorThree = Math.floor(Math.random() * 256);
     div.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgba( ${color} , ${colorTwo} , ${colorThree} )`;
-     box.append(div);
+    box.append(div);
   } 
 }

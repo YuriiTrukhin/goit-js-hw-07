b91e46556a6a31a290bfd694f07506a4d1183d7d
YuriiTrukhin <68285301+YuriiTrukhin@users.noreply.github.com>
2020-10-19 17:15:44 +0300

    поправил табуляцию

--- js/task-08.js
+++ js/task-08.js
@@ -18,8 +18,8 @@ function createBoxes(amount) {
     div = document.createElement("div");
     let size = basicSize + i * 10;
     let color = Math.floor(Math.random() * 256);
-  let colorTwo = Math.floor(Math.random() * 256);
-  let colorThree = Math.floor(Math.random() * 256);
+    let colorTwo = Math.floor(Math.random() * 256);
+    let colorThree = Math.floor(Math.random() * 256);
     div.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgba( ${color} , ${colorTwo} , ${colorThree} )`;
     box.append(div);
   } 
